419954ede8a41128444efe6ccac5f1998b256a13
kss <2538041184@qq.com>
2024-10-19 12:53:05 +0800

    fix：save

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-6">
-      <h1 className="text-4xl font-bold">水生资源社发布页</h1>
+      <h1 className="text-4xl font-bold">绝对の领域发布页</h1>
       <Link
         className="
         text-lg
@@ -17,7 +17,7 @@ export default function Home() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        主站 - 水生资源社
+        主站 - 绝对の领域
       </Link>
       <div>温馨提示： Ctrl + D 可以收藏本页面，以防迷路！</div>
     </main>
